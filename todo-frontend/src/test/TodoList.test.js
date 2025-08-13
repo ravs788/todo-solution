7b4966b497df8260f2023c0224d8b661327758aa
Ravi Shankar <ravs788@gmail.com>
2025-08-13 17:03:23 +0530

    fixed errors

--- todo-frontend/src/test/TodoList.test.js
+++ todo-frontend/src/test/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import TodoList from "./TodoList";
+import TodoList from "../components/TodoList";
 import AuthContext from "../context/AuthContext";
 
 // Mock data
